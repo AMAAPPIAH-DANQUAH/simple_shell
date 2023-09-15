24bc0c2986074ff4af906b7849c827a2ed5b8ad9
King-David Gyamfi Akuoko <akuokodavid@gmail.com>
2023-09-15 03:24:05 -0700

    to locate built-in functions

---
+++ exec_line.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * exec_line - Explore commands and built-in functions in shell.
+ *
+ * @datash: Pertinet data in shell (arguments).
+ * Return: 1 if successful
+ */
+
+
+int exec_line(data_shell *datash)
+{
+	int (*builtin)(data_shell *datash);
+
+	if (datash->args[0] == NULL)
+		return (1);
+
+	builtin = get_builtin(datash->args[0]);
+
+	if (builtin != NULL)
+		return (builtin(datash));
+
+	return (cmd_exec(datash));
+}
