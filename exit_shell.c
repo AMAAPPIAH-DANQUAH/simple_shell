97ae181ab5b99e4380ddd06b95489d3a172d0e63
King-David Gyamfi Akuoko <akuokodavid@gmail.com>
2023-09-15 03:24:44 -0700

    Function to exit shell

---
+++ exit_shell.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * exit_shell - As name implies, exits or closes the shell.
+ *
+ * @datash: Pertinent data structure containing status and arguments.
+ * Return: 0 if successfuly done.
+ */
+
+
+int exit_shell(data_shell *datash)
+{
+	unsigned int ustatus;
+	int is_digit;
+	int str_len;
+	int big_number;
+
+	if (datash->args[1] != NULL)
+	{
+		ustatus = _atoi(datash->args[1]);
+		is_digit = _isdigit(datash->args[1]);
+		str_len = _strlen(datash->args[1]);
+		big_number = ustatus > (unsigned int)INT_MAX;
+		if (!is_digit || str_len > 10 || big_number)
+		{
+			get_error(datash, 2);
+			datash->status = 2;
+			return (1);
+		}
+		datash->status = (ustatus % 256);
+	}
+	return (0);
+}
